4ff7881210daadd510a81db76a9a7df7e632f723
Sivarajan <citysiva180@users.noreply.github.com>
2024-07-20 09:42:22 +0530

    PYTHON CLEAN CODE | Initial Commit

---
+++ docstrings_example.py
@@ -0,0 +1,10 @@
+def do_something():
+     """run something with this function"""
+     return "hello world"
+
+print(do_something())
+
+do_something.__doc__ 
+# output - 'run something with this function'
+
+
